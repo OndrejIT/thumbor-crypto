ec608496084a7fbe405de2dc6ec4494b43eeb1d0
Ondřej Bárta <ondrej@ondrej.it>
2022-03-08 10:39:31 +0100

    Add crypto

--- thumbor_crypto/handler.py
+++ thumbor_crypto/handler.py
@@ -3,15 +3,61 @@
 # Copyright 2022
 
 import re
+from urllib.parse import unquote
+from Crypto.Cipher import AES
+import sys
+import zlib
+import base64
+import struct
+import hashlib
 
 from typing import Any
 
 from thumbor.url import Url
+from thumbor.utils import logger
+
 from thumbor.handler_lists import HandlerList
 from thumbor.handlers.imaging import ImagingHandler
-from thumbor.extension.crypto import url_decrypt
 
 url_compile = re.compile(Url.regex())
+
+
+def url_decrypt(uri, key):
+    """
+    Vygenerujeme inicializacni vektor z url + hesla prohnano md5
+    IV neni random aby fungovala kes prohlizece...
+    """
+    try:
+        url, query = uri.split("?")
+    except ValueError:
+        logger.error("[Decrypt error] Unauthorized url.")
+        return None
+
+    # FB query string url...
+    query = unquote(query)
+
+    hash_me = url + key
+    iv = hashlib.md5(hash_me.encode())
+    # Dorovname base 64 o =
+    padding = lambda s: s + "=" * (-len(s) % 4)
+
+    try:
+        decoded = base64.b64decode(padding(query))
+        crypt_object = AES.new(key=key.encode(), mode=AES.MODE_CBC, IV=iv.digest())
+        decrypted = crypt_object.decrypt(decoded[:-4]).decode().rstrip("\0")
+        checksum = zlib.crc32(decrypted.encode()) & 0xffffffff
+        pack = struct.pack("=L", checksum)
+        if not decoded[-4:] == pack:
+            raise Exception("Checksum mismatch.")
+
+        return decrypted
+    except:
+        e = sys.exc_info()[0]
+        logger.error(
+            f"[Decrypt error] Unauthorized url.\n{e}"
+        )
+
+        return None
 
 
 class CryptoImagingHandler(ImagingHandler):
